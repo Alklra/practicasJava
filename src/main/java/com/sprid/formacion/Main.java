b506b2c8023955a89fe3a5796c101c17c11aeec3
ariveral <alanrivelo@gmail.com>
2024-01-16 00:34:52 -0600

    Se crea repositorio

---
+++ src/main/java/com/sprid/formacion/Main.java
@@ -0,0 +1,10 @@
+package com.sprid.formacion;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String saludo = "Bienvenidos a las prácticas";
+        System.out.println(saludo);
+
+    }
+}
